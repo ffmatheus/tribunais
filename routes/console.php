b6df422fbe5cef85337e706dac1d337f1fad723d
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2019-01-15 13:39:44 -0200

    Fix routes

--- routes/console.php
+++ routes/console.php
@@ -1,19 +1,21 @@
 <?php
 
-use App\Services\Scrapers\TjRj\Scraper;
-use Illuminate\Foundation\Inspiring;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
+//use App\Services\Scrapers\TjRj\Scraper;
+//use Illuminate\Foundation\Inspiring;
+//
+///*
+//|--------------------------------------------------------------------------
+//| Console Routes
+//|--------------------------------------------------------------------------
+//|
+//| This file is where you may define all of your Closure based console
+//| commands. Each Closure is bound to a command instance allowing a
+//| simple approach to interacting with each command's IO methods.
+//|
+//*/
+//
+//Artisan::command('scrape', function () {
+//    app(Scraper::class)->scrape();
+//})->describe('Display an inspiring quote');
 
-Artisan::command('scrape', function () {
-    app(Scraper::class)->scrape();
-})->describe('Display an inspiring quote');
